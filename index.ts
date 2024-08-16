4732a8d4f5fba17ddadc7d3ae885a55e9d63cd39
Eddie McLean <middlerun@gmail.com>
2024-08-16 21:46:53 +1000

    Send alert when data couldn't be fetched

--- index.ts
+++ index.ts
@@ -57,6 +57,11 @@ async function run() {
 
   const updates = await getTripUpdates(relevantTripIds, log)
 
+  if (!updates) {
+    sendSMS('Could not check for train delays - could not fetch real-time data')
+    return
+  }
+
   if (updates.length === 0) {
     log('No updates found')
     return
